2e01f7075e34a93af051751947c122a7769a93fa
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-03-25 21:53:13 +0530

    Partials, Layouts and Assests added

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const port = 800;
 
+const expressLayouts = require('express-ejs-layouts');
+
+//In which folder should the app lookout for Static files
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+
+//Extract style and scripts from sub pages into the layout
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
 //Use express router
 
 app.use('/', require('./routes/index'));
