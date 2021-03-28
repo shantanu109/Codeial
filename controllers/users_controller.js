d65eb4e6da7b8a910cc9c953304256a0d9fc4408
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-03-28 14:46:53 +0530

    Manual Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,9 +4,31 @@ const User = require('../models/user');
 
 module.exports.profile = function(req,res){
 
-    return res.render('user_profile', {
-        title: "Users"
-    });
+    if (req.cookies.user_id){
+
+        User.findById(req.cookies.user_id, function(err, user){
+
+            if (user){
+
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user: user
+                })
+
+            }
+
+            return res.redirect('/users/sign-in');
+
+        });
+
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+
+
+
+    
 }
 
 //Render the Sign Up Page
@@ -68,5 +90,46 @@ module.exports.create = function(req,res){
 //Sign in and Create the session for User
 
 module.exports.createSession = function(req,res){
+
+    //Steps to Authenticate
+
+    //Find the User
+
+    User.findOne({email: req.body.email}, function(err,user){
+
+        if (err)
+        {
+            console.log('Error in finding user in Signing In');
+            return;
+        }
+
+        //Handle User Found
+
+        if(user)
+        {
+            //Handle Password which doesn't match
+
+            if (user.password != req.body.password)
+            {
+                return res.redirect('back');
+            }
+
+            //Handle Session Creation
+
+            res.cookie('user_id',user.id);
+
+            return res.redirect('/users/profile');
+
+        }
+        else
+        {
+            //Handle User Not Found
+
+            return res.redirect('back');
+
+        }
+
+    });
+
 
 }
