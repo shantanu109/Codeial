3614d3118d1fb3d538f19d59e47155ae8d641211
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-04-11 22:06:12 +0530

    Posts and Author added and viewed at Login

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,31 @@
+
+const Post = require('../models/post');
+
+
 module.exports.home = function(req,res){
     //return res.end('<h1>Express is up for codeial</h1>');
 
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
 
-    return res.render('home', {
-        title: "Home"
-    });
+    // Post.find({},function(err,posts){
+
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+
+    // });
+
+    //Populate the user of each post
+
+    Post.find({}).populate('user').exec(function(err,posts){
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+
+    })
+
 } 
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ console.log('Router Loaded');
 
 router.get('/', homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 //For any further routes, access from here
 //router.use('/routerName', require('./routerFile'));
