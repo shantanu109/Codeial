4ff94b5457d311f891afd96b76d92f447b30a948
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-04-12 22:49:51 +0530

    Deleting a Post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,9 +1,6 @@
-const Post = require("../models/post")
+const Post = require("../models/post");
 
-
-
-
-
+const Comment = require('../models/comment');
 
 
 module.exports.create = function(req,res){
@@ -18,6 +15,27 @@ module.exports.create = function(req,res){
         }
 
         return res.redirect('back');
+
+    });
+}
+
+
+module.exports.destroy = function(req,res){
+
+    Post.findById(req.params.id, function(err,post){
+
+        //.id means converting the object Id into string
+        if (post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+
+
+        } else {
+            return res.redirect('back');
+        }
 
     });
 }
