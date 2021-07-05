893c53ce83c72197839e406bb05ad46e68269f44
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-07-06 02:10:27 +0530

    Updated the signUp Api for Google Account SignUp

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -48,9 +48,19 @@ module.exports.signUp = async function (req, res) {
     }
 
     User.findOne({ email: req.body.email }, function (err, user) {
-      if (err) {
-        return res.json(500, {
-          message: "Internal Server Error",
+      if (user) {
+        return res.json(200, {
+          message: "Sign Up Successful, here is your token, plz keep it safe",
+
+          data: {
+            //user.JSON() part gets encrypted
+
+            token: jwt.sign(user.toJSON(), env.jwt_secret, {
+              expiresIn: "100000",
+            }),
+            user,
+          },
+          success: true,
         });
       }
 
