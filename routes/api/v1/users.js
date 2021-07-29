5393e64e845d3487d849c00da7e2905806ebccab
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-07-29 17:03:47 -0400

    Changed back

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const usersApi = require('../../../controllers/api/v1/users_api.js');
+const usersApi = require('../../../controllers/api/v1/users_api');
 const passport = require('passport');
 
 
